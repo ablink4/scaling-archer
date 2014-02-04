e52a9779c8d25ea51d1a30aa938f8f12181a8a92
root <root@famousamos.oppositionland>
2014-02-03 22:02:43 -0600

    Cleaned up code, modified to use blocking sockets because non-blocking were not finding ports that were open.  Todo: find a solution that will find open ports but wont take forever to time out, as the current solution does.

--- portmon/portmon.cpp
+++ portmon/portmon.cpp
@@ -1,18 +1,24 @@
+/*
+ * Author: ablink4
+ * 
+ * TODO list:
+ *   1. ping IP address before trying to connect to it
+ *   2. figure out how to reduce timeout on closed ports
+ * 
+ */ 
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
-
 #include <string.h> // memcpy()
-
 #include <unistd.h> // close()
-
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <netdb.h>
-
 #include <fcntl.h>
 #include <assert.h>
+#include <errno.h>
 
 #define LAST_PORT 65535 // largest valid port number
 
@@ -28,48 +34,46 @@ int main()
 {
 	int socket_fd = -1; 
 	
-	for(int i = 0; i <= LAST_PORT; i++)
-	{
+	struct sockaddr_in sock_info;
+	struct hostent *host_ptr;
 	
+	// using google as a test
+	const char *hostname = "www.google.com"; 
+	
+	host_ptr = gethostbyname(hostname);
+	
+	if(host_ptr == NULL)
+	{
+		std::cerr << strerror(errno) << std::endl;
+		close(socket_fd);
+		exit(-2);
+	}
+
+	memcpy((char*)&sock_info.sin_addr, host_ptr->h_addr, host_ptr->h_length);
+	sock_info.sin_family = AF_INET;
+	
+	for(int i = 1; i <= LAST_PORT; i++)
+	{	
 		socket_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
 		
 		if(socket_fd < 0)
 		{
-			perror("Could not create socket.\n");
+			std::cerr << strerror(errno) << std::endl;
 			exit(-1);
 		}
 		
-		set_nonblock(socket_fd);
-		
-		struct sockaddr_in sock_info;
-		struct hostent *host_ptr;
-		
-		// using google as a test
-		const char *hostname = "www.google.com"; 
-		
-		host_ptr = gethostbyname(hostname);
-		
-		if(host_ptr == NULL)
-		{
-			perror("Could not get host by name.\n");
-			close(socket_fd);
-			exit(-2);
-		}
+		//set_nonblock(socket_fd);
 
-		memcpy((char*)&sock_info.sin_addr, host_ptr->h_addr, host_ptr->h_length);
-		sock_info.sin_family = AF_INET;
-		
 		sock_info.sin_port = htons(i); 
 
 		if(connect(socket_fd, (struct sockaddr*)&sock_info, sizeof(sockaddr_in)) < 0)
 		{
-			perror("Could not connect socket.\n");
+			std::cerr << strerror(errno) << std::endl;
 			close(socket_fd);
-			//exit(-3);
 		}
 		else
 		{
-			printf("Port %d connected successfully!\n", i);
+			std::cout << "Port " << i << " connected successfully!" << std::endl;
 			close(socket_fd);
 		}
 	}
